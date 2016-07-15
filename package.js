4c8f5192212d805cb16de4d6da033264b9c23aec
ochicf <oscarchicfarre@gmail.com>
2016-07-15 17:46:44 +0200

    added git url

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'zetoff:astronomy-field-reference-behavior',
   version: '0.1.0',
   summary: 'Adds a field to an astronomy class that references to another document.',
-  git: '',
+  git: 'https://github.com/Zetoff/astronomy-field-reference-behavior',
   documentation: 'README.md'
 });
 
