e544173ee2c59a75f6cedc8e1f3805edd644332f
ochicf <oscarchicfarre@gmail.com>
2017-01-09 19:58:49 +0100

    update astronomy version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zetoff:astronomy-field-reference-behavior',
-  version: '0.3.0',
+  version: '0.4.0',
   summary: 'Adds a field to an astronomy class that references to other documents.',
   git: 'https://github.com/Zetoff/astronomy-field-reference-behavior',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.3.4.4');
   api.use('ecmascript');
-  api.use('jagi:astronomy@2.3.0');
+  api.use('jagi:astronomy@2.3.10');
   api.use('stevezhu:lodash@4.6.1');
   api.mainModule('astronomy-field-reference-behavior.js');
 });
@@ -19,7 +19,7 @@ Package.onTest(function(api) {
   api.use('mongo');
   api.use('practicalmeteor:mocha');
   api.use('practicalmeteor:chai');
-  api.use('jagi:astronomy@2.3.0');
+  api.use('jagi:astronomy@2.3.10');
   api.use('stevezhu:lodash@4.6.1')
   api.use('zetoff:astronomy-field-reference-behavior');
   api.mainModule('astronomy-field-reference-behavior-tests.js');
